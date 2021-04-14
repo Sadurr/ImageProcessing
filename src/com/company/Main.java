7407e5e436e61f6d55394b79e5eaafa107405c92
Sadurr <filipsadurski3@gmail.com>
2021-04-14 23:28:13 +0200

    server now uses ImageProcessing class which is responsible for changing image saturation

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            Server server = new Server();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
